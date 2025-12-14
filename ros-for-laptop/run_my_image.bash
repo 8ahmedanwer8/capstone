c9d6f291dfd10cc5d49b56d209f3af6d40c248ed
Ahmed Anwer <sahmedanwer@gmail.com>
2025-12-14 03:36:36 -0500

    update ros laptop dockerfile

--- ros-for-laptop/run_my_image.bash
+++ ros-for-laptop/run_my_image.bash
@@ -1,23 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE_NAME="my-ros-melodic-full"
+
 XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]
-then
-    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
-    if [ ! -z "$xauth_list" ]
-    then
-        echo $xauth_list | xauth -f $XAUTH nmerge -
-    else
-        touch $XAUTH
-    fi
-    chmod a+r $XAUTH
+if [ ! -f "$XAUTH" ]; then
+  xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+  if [ -n "$xauth_list" ]; then
+    echo "$xauth_list" | xauth -f "$XAUTH" nmerge -
+  else
+    touch "$XAUTH"
+  fi
+  chmod a+r "$XAUTH"
 fi
 
-docker run -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --volume="slam_ws:/root/ros_ws" \
-    --env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XAUTH" \
-    --runtime=nvidia \
-    my-ros-melodic-full \
-    bash
+docker run -it --rm \
+  --name ros-melodic-laptop \
+  --net=host \
+  --env="DISPLAY=$DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --env="XAUTHORITY=$XAUTH" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="$XAUTH:$XAUTH" \
+  --volume="slam_ws:/root/ros_ws" \
+  --runtime=nvidia \
+  "$IMAGE_NAME" \
+  bash
